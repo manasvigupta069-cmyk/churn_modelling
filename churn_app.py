277ff370a72e546f022b700c7cffb522121a94c8
manasvigupta069-cmyk <manasvigupta069@gmail.com>
2026-07-09 21:21:04 +0530

    Update churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -12,3 +12,27 @@ Geography= st.selectbox("Geography",("France","Germany","Spain"))
 Gender= st.selectbox("Gender",("Male","Female"))
 Has_credit_card= st.selectbox("Has Credit Card",("Yes","No"))
 Is_active_member= st.selectbox("Is Active Member",("Yes","No"))
+model = tf.keras.models.load_model("churn_class.h5")
+
+geo = {"France": 0, "Germany": 1, "Spain": 2}
+gender = {"Male": 0, "Female": 1}
+yes_no = {"Yes": 1, "No": 0}
+
+if st.button("Predict"):
+    input_data = np.array([[
+        CS,
+        Age,
+        Tenure,
+        Balance,
+        NumOfProducts,
+        geo[Geography],
+        gender[Gender],
+        yes_no[Is_active_member]
+    ]])
+
+    prediction = model.predict(input_data)
+
+    if prediction[0][0] > 0.5:
+        st.error("Customer is likely to Churn")
+    else:
+        st.success("Customer is likely to Stay")
